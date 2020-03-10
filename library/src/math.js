8145f157b785ed35d8086890c3b50add96cfa46d
lynnelyin <1440353878@qq.com>
2020-03-10 14:48:33 +0800

    library

---
+++ library/src/math.js
@@ -0,0 +1,15 @@
+export function add(x, y) {
+  return x + y
+}
+
+export function minus(x, y) {
+  return x - y
+}
+
+export function multiply(x, y) {
+  return x * y
+}
+
+export function division(x, y) {
+  return x / y
+}
